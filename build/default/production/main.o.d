d1afe3b9cfd555b8442721a58882d5692a167091
compooo <ahmed0142005944@gmail.com>
2021-09-11 13:18:02 +0200

    comit

---
+++ build/default/production/main.o.d
@@ -0,0 +1,31 @@
+build/default/production/main.o.d build/default/production/main.o: main.c \
+ e:\xc8\avr\avr\include\avr\io.h e:\xc8\avr\avr\include\avr\sfr_defs.h \
+ e:\xc8\avr\avr\include\inttypes.h \
+ e:\xc8\avr\lib\gcc\avr\5.4.0\include\stdint.h \
+ e:\xc8\avr\avr\include\stdint.h \
+ E:/mplab/packs/Microchip/ATmega_DFP/2.3.126/xc8/avr/include/avr/iom32.h \
+ e:\xc8\avr\avr\include\avr\portpins.h \
+ e:\xc8\avr\avr\include\avr\common.h e:\xc8\avr\avr\include\avr\version.h \
+ e:\xc8\avr\avr\include\avr\fuse.h e:\xc8\avr\avr\include\avr\lock.h
+
+e:\xc8\avr\avr\include\avr\io.h:
+
+e:\xc8\avr\avr\include\avr\sfr_defs.h:
+
+e:\xc8\avr\avr\include\inttypes.h:
+
+e:\xc8\avr\lib\gcc\avr\5.4.0\include\stdint.h:
+
+e:\xc8\avr\avr\include\stdint.h:
+
+E:/mplab/packs/Microchip/ATmega_DFP/2.3.126/xc8/avr/include/avr/iom32.h:
+
+e:\xc8\avr\avr\include\avr\portpins.h:
+
+e:\xc8\avr\avr\include\avr\common.h:
+
+e:\xc8\avr\avr\include\avr\version.h:
+
+e:\xc8\avr\avr\include\avr\fuse.h:
+
+e:\xc8\avr\avr\include\avr\lock.h:
---
+++ main.c
@@ -0,0 +1,15 @@
+/*
+ * File:   main.c
+ * Author: ahmed
+ *
+ * Created on September 11, 2021, 1:12 PM
+ */
+
+
+#include <avr/io.h>
+
+int main(void) {
+    /* Replace with your application code */
+    while (1) {
+    }
+}
